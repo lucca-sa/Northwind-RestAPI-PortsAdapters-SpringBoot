68137862128da73202449f2cfa4b1db9b23a2a1e
lucca-sa <scherer.lucca@gmail.com>
2024-05-16 12:25:36 -0300

    feat: Field validation for edit a customer endpoint

--- src/main/java/com/hexagonal/restapi/adapter/input/customers/dto/EditarClienteRequest.java
+++ src/main/java/com/hexagonal/restapi/adapter/input/customers/dto/EditarClienteRequest.java
@@ -2,6 +2,8 @@ package com.hexagonal.restapi.adapter.input.customers.dto;
 
 import org.springframework.stereotype.Component;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,14 +15,34 @@ import lombok.Setter;
 @Getter
 @Setter
 public class EditarClienteRequest {
+    @NotNull(message = "O campo companyName é obrigatório.")
+    @Size(min = 1, max = 40, message = "O companyName deve ser uma string de até 40 caracteres.")
     private String companyName;
-    private String phone;
+
+    @Size(min = 1, max = 40, message = "O contactName deve ser uma string de até 30 caracteres.")
     private String contactName;
+
+    @Size(min = 1, max = 40, message = "O phone deve ser uma string de até 24 caracteres.")
+    private String phone;
+
+    @Size(min = 1, max = 40, message = "O contactTitle deve ser uma string de até 30 caracteres.")
     private String contactTitle;
+
+    @Size(min = 1, max = 40, message = "O address deve ser uma string de até 60 caracteres.")
     private String address;
+
+    @Size(min = 1, max = 40, message = "O city deve ser uma string de até 15 caracteres.")
     private String city;
+
+    @Size(min = 1, max = 40, message = "O region deve ser uma string de até 15 caracteres.")
     private String region;
+
+    @Size(min = 1, max = 40, message = "O postalCode deve ser uma string de até 10 caracteres.")
     private String postalCode;
+
+    @Size(min = 1, max = 40, message = "O country deve ser uma string de até 15 caracteres.")
     private String country;
+
+    @Size(min = 1, max = 40, message = "O fax deve ser uma string de até 24 caracteres.")
     private String fax;
 }
