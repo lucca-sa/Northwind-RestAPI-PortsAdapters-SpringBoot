c85f1ef6243874f31afbd5dd13df192ccd23857c
lucca-sa <scherer.lucca@gmail.com>
2024-05-16 12:27:37 -0300

    fix: All good no one got hurt ;) (would i lose my job over this?)

--- src/main/java/com/hexagonal/restapi/adapter/input/customers/dto/EditarClienteRequest.java
+++ src/main/java/com/hexagonal/restapi/adapter/input/customers/dto/EditarClienteRequest.java
@@ -19,30 +19,30 @@ public class EditarClienteRequest {
     @Size(min = 1, max = 40, message = "O companyName deve ser uma string de até 40 caracteres.")
     private String companyName;
 
-    @Size(min = 1, max = 40, message = "O contactName deve ser uma string de até 30 caracteres.")
+    @Size(min = 1, max = 30, message = "O contactName deve ser uma string de até 30 caracteres.")
     private String contactName;
 
-    @Size(min = 1, max = 40, message = "O phone deve ser uma string de até 24 caracteres.")
+    @Size(min = 1, max = 24, message = "O phone deve ser uma string de até 24 caracteres.")
     private String phone;
 
-    @Size(min = 1, max = 40, message = "O contactTitle deve ser uma string de até 30 caracteres.")
+    @Size(min = 1, max = 30, message = "O contactTitle deve ser uma string de até 30 caracteres.")
     private String contactTitle;
 
-    @Size(min = 1, max = 40, message = "O address deve ser uma string de até 60 caracteres.")
+    @Size(min = 1, max = 60, message = "O address deve ser uma string de até 60 caracteres.")
     private String address;
 
-    @Size(min = 1, max = 40, message = "O city deve ser uma string de até 15 caracteres.")
+    @Size(min = 1, max = 15, message = "O city deve ser uma string de até 15 caracteres.")
     private String city;
 
-    @Size(min = 1, max = 40, message = "O region deve ser uma string de até 15 caracteres.")
+    @Size(min = 1, max = 15, message = "O region deve ser uma string de até 15 caracteres.")
     private String region;
 
-    @Size(min = 1, max = 40, message = "O postalCode deve ser uma string de até 10 caracteres.")
+    @Size(min = 1, max = 10, message = "O postalCode deve ser uma string de até 10 caracteres.")
     private String postalCode;
 
-    @Size(min = 1, max = 40, message = "O country deve ser uma string de até 15 caracteres.")
+    @Size(min = 1, max = 15, message = "O country deve ser uma string de até 15 caracteres.")
     private String country;
 
-    @Size(min = 1, max = 40, message = "O fax deve ser uma string de até 24 caracteres.")
+    @Size(min = 1, max = 24, message = "O fax deve ser uma string de até 24 caracteres.")
     private String fax;
 }
